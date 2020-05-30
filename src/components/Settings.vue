85892619c840a4c446d51c489fc370151a6008d5
disgraceful <noolic97@gmail.com>
2020-05-30 19:01:28 +0300

    Settings menu time to seconds

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -5,26 +5,73 @@
         <v-col cols="auto">
           <v-row>
             <input
-              v-model="work.minutes"
+              v-model.number="work.minutes"
               maxlength="3"
-              @input="inputHandler($event)"
+              @focus="inputTempValue = work.minutes"
+              @input="inputHandler($event, minutesValidSymbolLimit)"
               @blur="work.minutes = inputTempValue"
             />
             <span id="semi">:</span>
             <input
               v-model="work.seconds"
               maxlength="2"
-              @input="inputHandler($event)"
+              @focus="inputTempValue = work.seconds"
+              @input="inputHandler($event,secondsValidSymbolLimit)"
               @blur="work.seconds = inputTempValue"
             />
           </v-row>
           <label>Work Time</label>
         </v-col>
       </v-row>
-      <input id="break" />
-      <label for="break">Break</label>
-      <input id="longbreak" />
-      <label for="longbreak">Long break</label>
+      <v-row justify="center">
+        <v-col cols="auto">
+          <v-row align="center" justify="center">
+            <input
+              class="small-input"
+              v-model.number="rest.minutes"
+              maxlength="3"
+              @focus="inputTempValue = rest.minutes"
+              @input="inputHandler($event, minutesValidSymbolLimit)"
+              @blur="rest.minutes = inputTempValue"
+            />
+            <span id="semi">:</span>
+            <input
+              class="small-input"
+              v-model="rest.seconds"
+              maxlength="2"
+              @focus="inputTempValue = rest.seconds"
+              @input="inputHandler($event,secondsValidSymbolLimit)"
+              @blur="rest.seconds = inputTempValue"
+            />
+            <label>Break</label>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="auto">
+          <v-row align="center" justify="center">
+            <input
+              class="small-input"
+              v-model.number="long.minutes"
+              maxlength="3"
+              @focus="inputTempValue = long.minutes"
+              @input="inputHandler($event, minutesValidSymbolLimit)"
+              @blur="long.minutes = inputTempValue"
+            />
+            <span id="semi">:</span>
+            <input
+              class="small-input"
+              v-model="long.seconds"
+              maxlength="2"
+              @focus="inputTempValue = long.seconds"
+              @input="inputHandler($event,secondsValidSymbolLimit)"
+              @blur="long.seconds = inputTempValue"
+            />
+            <label>long break</label>
+          </v-row>
+        </v-col>
+      </v-row>
+
       <v-row>
         <v-col>
           <v-btn @click="save()">Save</v-btn>
@@ -50,7 +97,7 @@ export default {
         minutes: 0,
         seconds: 0
       },
-      break: {
+      rest: {
         minutes: 0,
         seconds: 0
       },
@@ -58,24 +105,28 @@ export default {
         minutes: 0,
         seconds: 0
       },
-      inputTempValue: 0
+      inputTempValue: 0,
+      minutesValidSymbolLimit: 1,
+      secondsValidSymbolLimit: 2
     };
   },
   methods: {
-    inputHandler(event) {
-      if (!isNaN(event.target.value)) {
+    inputHandler(event, limit) {
+      console.dir(event.target);
+      if (!isNaN(event.target.value) && event.target.value.length >= limit) {
         this.inputTempValue = event.target.value;
       }
     },
     minutesToSecs(minutes, seconds) {
-      return minutes * 60 + seconds;
+      return parseInt(minutes) * 60 + parseInt(seconds);
     },
     retrieve() {
       chrome.storage.sync.get("settings", result => {
         let workTime, breakTime, longTime;
+        console.log(result);
         if (result.settings) {
           workTime = this.calcTimeMinutes(result.settings.work);
-          breakTime = this.calcTimeMinutes(result.settings.break);
+          breakTime = this.calcTimeMinutes(result.settings.rest);
           longTime = this.calcTimeMinutes(result.settings.long);
         } else {
           workTime = this.calcTimeMinutes(this.defaultTimes[0]);
@@ -85,8 +136,8 @@ export default {
 
         this.work.minutes = workTime.minutes;
         this.work.seconds = workTime.seconds;
-        this.break.minutes = breakTime.minutes;
-        this.break.seconds = breakTime.seconds;
+        this.rest.minutes = breakTime.minutes;
+        this.rest.seconds = breakTime.seconds;
         this.long.minutes = longTime.minutes;
         this.long.seconds = longTime.seconds;
       });
@@ -95,14 +146,14 @@ export default {
       chrome.storage.sync.set({
         settings: {
           work: this.minutesToSecs(this.work.minutes, this.work.seconds),
-          break: this.minutesToSecs(this.break.minutes, this.break.seconds),
+          rest: this.minutesToSecs(this.rest.minutes, this.rest.seconds),
           long: this.minutesToSecs(this.long.minutes, this.long.seconds)
         }
       });
     },
     reset() {
       chrome.storage.sync.clear();
-      this.save();
+      this.retrieve();
     }
   },
   created() {
@@ -112,8 +163,8 @@ export default {
 </script>
 
 <style>
-div {
-  overflow: hidden;
+html {
+  overflow-y: hidden !important;
 }
 
 input {
@@ -121,6 +172,11 @@ input {
   font-weight: 300 !important;
   max-height: 80px !important;
   width: 100px;
+}
+
+.small-input {
+  font-size: 30px !important;
+  width: 50px;
 }
 
 input:nth-child(1) {
@@ -134,6 +190,11 @@ input:focus {
 
 #semi {
   font-size: 50px;
+  margin: 2px;
+}
+
+.small-input + #semi {
+  font-size: 28px;
 }
 </style>
 
