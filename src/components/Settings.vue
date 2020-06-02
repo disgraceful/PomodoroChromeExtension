ce9e938d2c211eddc1e297d5430dfa9e560a42fd
disgraceful <noolic97@gmail.com>
2020-06-02 18:43:58 +0300

    Expand settings options

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -2,76 +2,96 @@
   <v-card>
     <v-container class="text-center">
       <v-row justify="center">
-        <v-col cols="auto">
-          <v-row>
-            <input
-              v-model.number="work.minutes"
-              maxlength="3"
-              @focus="inputTempValue = work.minutes"
-              @input="inputHandler($event, minutesValidSymbolLimit)"
-              @blur="work.minutes = inputTempValue"
-            />
-            <span id="semi">:</span>
-            <input
-              v-model="work.seconds"
-              maxlength="2"
-              @focus="inputTempValue = work.seconds"
-              @input="inputHandler($event,secondsValidSymbolLimit)"
-              @blur="work.seconds = inputTempValue"
-            />
-          </v-row>
-          <label>Work Time</label>
+        <input
+          type="text"
+          v-model.number="work.minutes"
+          maxlength="3"
+          @focus="inputTempValue = work.minutes"
+          @input="inputHandler($event, minutesValidSymbolLimit)"
+          @blur="work.minutes = inputTempValue"
+        />
+        <span id="semi">:</span>
+        <input
+          type="text"
+          v-model="work.seconds"
+          maxlength="2"
+          @focus="inputTempValue = work.seconds"
+          @input="inputHandler($event,secondsValidSymbolLimit)"
+          @blur="work.seconds = inputTempValue"
+        />
+      </v-row>
+      <label class="title font-weight-light">Work Time</label>
+      <v-row align="baseline" justify="center">
+        <input
+          type="text"
+          class="small-input"
+          v-model.number="rest.minutes"
+          maxlength="3"
+          @focus="inputTempValue = rest.minutes"
+          @input="inputHandler($event, minutesValidSymbolLimit)"
+          @blur="rest.minutes = inputTempValue"
+        />
+        <span id="semi">:</span>
+        <input
+          type="text"
+          class="small-input"
+          v-model="rest.seconds"
+          maxlength="2"
+          @focus="inputTempValue = rest.seconds"
+          @input="inputHandler($event,secondsValidSymbolLimit)"
+          @blur="rest.seconds = inputTempValue"
+        />
+        <label class="title font-weight-light">break</label>
+      </v-row>
+      <v-row justify="center" align="baseline">
+        <div style="margin-left:40px">
+          <input
+            type="text"
+            class="small-input"
+            v-model.number="long.minutes"
+            maxlength="3"
+            @focus="inputTempValue = long.minutes"
+            @input="inputHandler($event, minutesValidSymbolLimit)"
+            @blur="long.minutes = inputTempValue"
+          />
+          <span id="semi">:</span>
+          <input
+            type="text"
+            class="small-input"
+            v-model="long.seconds"
+            maxlength="2"
+            @focus="inputTempValue = long.seconds"
+            @input="inputHandler($event,secondsValidSymbolLimit)"
+            @blur="long.seconds = inputTempValue"
+          />
+          <label class="title font-weight-light">long break</label>
+        </div>
+      </v-row>
+      <hr />
+      <v-row align="baseline">
+        <v-col cols="auto" class="pb-0">
+          <span class="title font-weight-light">Long break every</span>
+          <input type="text" class="small-input text-center" v-model="workCycle" maxlength="1" />
+          <span class="title font-weight-light">sessions</span>
         </v-col>
       </v-row>
-      <v-row justify="center">
-        <v-col cols="auto">
-          <v-row align="center" justify="center">
-            <input
-              class="small-input"
-              v-model.number="rest.minutes"
-              maxlength="3"
-              @focus="inputTempValue = rest.minutes"
-              @input="inputHandler($event, minutesValidSymbolLimit)"
-              @blur="rest.minutes = inputTempValue"
-            />
-            <span id="semi">:</span>
-            <input
-              class="small-input"
-              v-model="rest.seconds"
-              maxlength="2"
-              @focus="inputTempValue = rest.seconds"
-              @input="inputHandler($event,secondsValidSymbolLimit)"
-              @blur="rest.seconds = inputTempValue"
-            />
-            <label>Break</label>
-          </v-row>
+      <v-row align="baseline">
+        <v-col cols="auto" class="pb-0">
+          <span class="title font-weight-light">Auto-resume timer:</span>
+        </v-col>
+        <v-col class="pa-0">
+          <v-checkbox v-model="autoResume" hide-details color="#333"></v-checkbox>
         </v-col>
       </v-row>
-      <v-row justify="center">
-        <v-col cols="auto">
-          <v-row align="center" justify="center">
-            <input
-              class="small-input"
-              v-model.number="long.minutes"
-              maxlength="3"
-              @focus="inputTempValue = long.minutes"
-              @input="inputHandler($event, minutesValidSymbolLimit)"
-              @blur="long.minutes = inputTempValue"
-            />
-            <span id="semi">:</span>
-            <input
-              class="small-input"
-              v-model="long.seconds"
-              maxlength="2"
-              @focus="inputTempValue = long.seconds"
-              @input="inputHandler($event,secondsValidSymbolLimit)"
-              @blur="long.seconds = inputTempValue"
-            />
-            <label>long break</label>
-          </v-row>
+      <v-row align="baseline">
+        <v-col cols="auto" class="pb-0">
+          <span class="title font-weight-light">Turn notifications:</span>
+        </v-col>
+        <v-col cols="auto" class="pa-0">
+          <v-checkbox v-model="notifications" hide-details color="#333"></v-checkbox>
         </v-col>
       </v-row>
-
+      <hr />
       <v-row>
         <v-col>
           <v-btn @click="save()">Save</v-btn>
@@ -93,6 +113,7 @@ export default {
   data() {
     return {
       defaultTimes: [20.5 * 60, 5 * 60, 10 * 60],
+      defaultSettings: { workCycle: 4, autoResume: true, notifications: true },
       work: {
         minutes: 0,
         seconds: 0
@@ -107,7 +128,10 @@ export default {
       },
       inputTempValue: 0,
       minutesValidSymbolLimit: 1,
-      secondsValidSymbolLimit: 2
+      secondsValidSymbolLimit: 2,
+      workCycle: 4,
+      autoResume: true,
+      notifications: true
     };
   },
   methods: {
@@ -140,6 +164,12 @@ export default {
         this.rest.seconds = breakTime.seconds;
         this.long.minutes = longTime.minutes;
         this.long.seconds = longTime.seconds;
+        this.workCycle =
+          result.settings.workCycle | this.defaultSettings.workCycle;
+        this.autoResume =
+          result.settings.autoResume | this.defaultSettings.autoResume;
+        this.notifications =
+          result.settings.notifications | this.defaultSettings.notifications;
       });
     },
     save() {
@@ -147,7 +177,10 @@ export default {
         settings: {
           work: this.minutesToSecs(this.work.minutes, this.work.seconds),
           rest: this.minutesToSecs(this.rest.minutes, this.rest.seconds),
-          long: this.minutesToSecs(this.long.minutes, this.long.seconds)
+          long: this.minutesToSecs(this.long.minutes, this.long.seconds),
+          workCycle: this.workCycle,
+          autoResume: this.autoResume,
+          notifications: this.notifications
         }
       });
     },
@@ -167,16 +200,16 @@ html {
   overflow-y: hidden !important;
 }
 
-input {
-  font-size: 60px !important;
-  font-weight: 300 !important;
-  max-height: 80px !important;
+input[type="text"] {
+  font-size: 60px;
+  font-weight: 300;
+  max-height: 80px;
   width: 100px;
 }
 
-.small-input {
+input.small-input {
   font-size: 30px !important;
-  width: 50px;
+  width: 40px;
 }
 
 input:nth-child(1) {
@@ -196,6 +229,9 @@ input:focus {
 .small-input + #semi {
   font-size: 28px;
 }
-</style>
 
+.v-input--selection-controls {
+  margin-top: -5px !important;
+}
+</style>
 
